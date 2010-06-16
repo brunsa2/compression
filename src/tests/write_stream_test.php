2a33238b26b939c1c003116df4240aadf16f93bb
brunsa2 <brunsa2@gmail.com>
2010-06-16 16:32:15 -0500

    finished more of WriteStream unit tests

--- src/tests/write_stream_test.php
+++ src/tests/write_stream_test.php
@@ -6,6 +6,7 @@ require_once('../write_stream.php');
 class WriteStreamTest extends PHPUnit_Framework_TestCase {
 	public function testWriteInt() {
 		$stream = new WriteStream();
+		
 		$stream->writeInt(65);
 		$this->assertEquals((string) $stream, 'A');
 	}
@@ -13,6 +14,26 @@ class WriteStreamTest extends PHPUnit_Framework_TestCase {
 	public function testWrite() {
 		$stream = new WriteStream();
 		
+		$stream->write(array('A', 'B', 'C'));
+		$this->assertEquals((string) $stream, 'ABC');
+		
+		$stream->write(array('A', 'B', 'C'), 1);
+		$this->assertEquals((string) $stream, 'ABCBC');
+		
+		$stream->write(array('A', 'B', 'C'), 1, 1);
+		$this->assertEquals((string) $stream, 'ABCBCB');
+		
+		$stream->write('ABC');
+		$stream->assertEquals((string) $stream, 'ABCBCBABC');
+		
+		$stream->write('ABC', 1);
+		$stream->assertEquals((string) $stream, 'ABCBCBABCBC');
+		
+		$stream->write('ABC', 1, 1);
+		$stream->assertEquals((string) $stream, 'ABCBCBABCBCB');
+		
+		$stream->write(123);
+		$stream->assertEquals((string) $stream, 'ABCBCBABCBCB123');
 	}
 }
 
