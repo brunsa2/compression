625a0f88971a6b39ae90e3673442de6533eefbb0
brunsa2 <brunsa2@gmail.com>
2010-06-16 14:07:45 -0500

    started documentation

--- write_stream.php
+++ write_stream.php
@@ -1,20 +1,58 @@
 <?php
 
+/**
+ * write_stream.php contains class {@link WriteStream}
+ *
+ * @author Jeff Stubler
+ * @version 1.0
+ * @package com.jeffstubler.streams;
+ */
+
+/**
+ * {@code WriteStream} provides a sequential buffer in which different data types can be
+ * appended. The entire buffer can be accessed as a string.
+ *
+ * @author Jeff Stubler
+ * @version 1.0
+ * @package com.jeffstubler.streams;
+ */
+
 class WriteStream {
+	/**
+	 * Internal data buffer.
+	 */
 	protected $streamData;
+	
+	/**
+	 * Index of last item in buffer.
+	 */
 	protected $endPointer;
 	
+	/**
+	 * Creates a new WriteStream object.
+	 */
 	public function __construct() {
 		$this->streamData = array();
 		$this->endPointer = 0;
 	}
 	
+	/**
+	 * Write a number to the stream. Only the least significant byte is written.
+	 * 
+	 * @param mixed $data Number to write to the stream.
+	 */
 	public function writeInt($data = 0) {
 		$data = (integer) $data & 0xff;
 		
 		$this->streamData[$this->endPointer++] = $data;
 	}
 	
+	/**
+	 * Write data to the stream. Data is written as a string.
+	 *
+	 * @param mixed $data Data to write to the stream;
+	 * @param integer $offset Starting position in 
+	 */
 	public function write($data = null, $offset = 0, $length = null) {
 		if(gettype($data) == 'array') {
 			if($length == null) {
