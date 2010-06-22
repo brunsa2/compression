05f53a6b14ce6eee674f017da10761e0dddb2a02
brunsa2 <brunsa2@gmail.com>
2010-06-22 17:07:07 -0500

    wrote a unit test for compression and decompression; doesn't seem to work right, so will attempt to fix the streams

---
+++ src/tests/CompressionTest.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('PHPUnit/Framework.php');
+require_once('../CompressionStream.php');
+require_once('../DecompressionStream.php');
+require_once('../RangeEncoder.php');
+require_once('../RangeDecoder.php');
+require_once('../Order0Model.php');
+require_once('../ReadStream.php');
+require_once('../WriteStream.php');
+
+class CompressionTest extends PHPUnit_Framework_TestCase {
+	public function testCompression() {
+		$compressionStream = new CompressionStream(false);
+		$compressionStream->write('The quick brown fox jumps over a lazy dog.');
+		$decompressionStream = new DecompressionStream(new ReadStream($compressionStream), false);
+		//echo $compressionStream . '<br />';
+		//echo $decompressionStream->getStream() . '<br />';
+		$this->assertEquals('The quick brown fox jumps over a lazy dog.', (string) $decompressionStream->getStream());
+		
+		if(strlen((string) $compressionStream) > strlen('The quick brown fox jumps over a lazy dog.')) {
+			echo 'Potential problem: compressed size greater than original size.';
+		}
+	}
+}
+
+//$ct = new CompressionTest();
+//$ct->testCompression();
+
+?>
